5625e217d06f4565773186a3345d64bc00e1b214
Jomar Delos Reyes <jomardelosreyes32@gmail.com>
2024-04-09 01:23:32 +0800

    feat: add performTransaction

--- server/index.js
+++ server/index.js
@@ -4,9 +4,10 @@ const app = express();
 const cors = require("cors");
 const PORT = process.env.PORT;
 const {connectNode} = require('./nodes.js');
+const {makeTransaction, setIsolationLevel} = require('./db.js');
 
 const fetchData = async (query) => {
-        const centralNodeConnection = await connectNode("1");
+        const centralNodeConnection = await connectNode(1);
         if(centralNodeConnection) {
             //master is working
             try {
@@ -25,8 +26,8 @@ const fetchData = async (query) => {
 
         } else {
             // if master fails, build database from node2 and node3
-            const Node2Connection = await connectNode("2");
-            const Node3Connection = await connectNode("3");
+            const Node2Connection = await connectNode(2);
+            const Node3Connection = await connectNode(3);
 
             try {
                 let node2rows = null;
@@ -69,6 +70,39 @@ const fetchData = async (query) => {
         }
 };
 
+const performTransaction = async (query, apptid) => {
+    const centralNodeConnection = await connectNode(1);
+    await setIsolationLevel(centralNodeConnection, "READ UNCOMMITTED");
+
+    if (centralNodeConnection) {
+        try {
+            return await makeTransaction(centralNodeConnection, 1, query, apptid);
+          } catch (err) {
+            console.log(err);
+          }
+
+    } else {
+        const Node2Connection = await connectNode(2);
+        const Node3Connection = await connectNode(3);
+
+        if (regionname === "Luzon") {
+            try {
+                await setIsolationLevel(Node2Connection, "READ UNCOMMITTED");
+                return await makeTransaction(Node2Connection, 2, query, apptid);
+            } catch(err) {
+                console.log(err);
+            }
+        } else {
+            try {
+                await setIsolationLevel(Node3Connection, "READ UNCOMMITTED");
+                return await makeTransaction(Node3Connection, 3, query, apptid);
+            } catch(err) {
+                console.log(err);
+            }
+        }   
+    }
+}
+
 const searchQuery = async (node, query) => {
     const connectedNode = await connectNode(node);
     if(connectNode) {
@@ -107,8 +141,6 @@ app.get("/api/view", async (req, res) => {
 })
 
 app.post("/api/submitDevOptions", async (req, res) => {
-    console.log(req.body.node)
-    console.log(req.body.query)
    try {
     const data = await searchQuery(req.body.node, req.body.query);
     if (data) {
@@ -123,7 +155,6 @@ app.post("/api/submitDevOptions", async (req, res) => {
 
 app.post("/api/update", async (req, res) => {
     try {
-        console.log(req.body)
         const apptid = req.body.apptid;
         const pxid = req.body.pxid;
         const clinicid = req.body.clinicid;
@@ -133,13 +164,14 @@ app.post("/api/update", async (req, res) => {
         const queuedate = new Date(req.body.queuedate)
         const starttime = new Date(req.body.starttime)
         const endtime = new Date(req.body.endtime)
+        const query = {
+            statement: "UPDATE appointments SET pxid = ?, clinicid = ?, regionname = ?, status = ?, timequeued = ?, queuedate = ?, starttime = ?, endtime = ? WHERE apptid = ?",
+            value: [pxid, clinicid, regionname, status, timequeued, queuedate, starttime, endtime, apptid],
+            type: "UPDATE",
+        }
 
+        const result = await performTransaction(query, apptid);
         
-        const [result] = await queryNode("1", 
-        "UPDATE appointments SET pxid = ?, clinicid = ?, regionname = ?, status = ?, timequeued = ?, queuedate = ?, starttime = ?, endtime = ? WHERE apptid = ?",
-        [pxid, clinicid, regionname, status, timequeued, queuedate, starttime, endtime, apptid]
-        );
-
         if (result.affectedRows > 0) {
             res.status(200).json({ message: "Data updated successfully." });
         } else {
@@ -149,21 +181,26 @@ app.post("/api/update", async (req, res) => {
     } catch (error) {
         console.error("Error updating data:", error);
         res.status(500).json({ message: "Error updating data." });
-    }
+    } 
 });
 
 app.post("/api/delete", async (req, res) => {
     try {
         const apptid = req.body.apptid;
+        const query = {
+            statement: "DELETE FROM appointments WHERE apptid = ?",
+            value: [apptid],
+            type: "DELETE",
+        };
+    
+        const result = await performTransaction(query, apptid);
 
-        await pool.query(
-            "DELETE FROM appointments WHERE apptid = ?",
-            [apptid]
-        );
-
-        res.status(200).json({ message: "Data deleted successfully." });
-
-    } catch (error) {
+        if (result.affectedRows > 0) {
+            res.status(200).json({ message: "Data deleted successfully." });
+        } else {
+            res.status(404).json({ message: "Record not found." });
+        }
+    } catch(error) {
         console.error("Error deleting data:", error);
         res.status(500).json({ message: "Error deleting data." });
     }
