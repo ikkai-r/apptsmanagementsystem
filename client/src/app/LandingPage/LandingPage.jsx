582499338e7d84e2a15d3745136745119e05fdc2
Jomar Delos Reyes <jomardelosreyes32@gmail.com>
2024-04-08 20:35:43 +0800

    refactor: move DevOptions page directory

--- client/src/app/LandingPage/LandingPage.jsx
+++ client/src/app/LandingPage/LandingPage.jsx
@@ -5,7 +5,7 @@ export default function LandingPage() {
     return (
         <section id="LandingPage" className="flex flex-col pt-16 min-h-screen w-full">
                     <div className="bg-emerald-400 p-2 rounded-md font-bold w-52 mb-10">
-                    <Link href="/test" className="text-2xl">Go to test page</Link>
+                    <Link href="/DevPage" className="text-2xl">Go to test page</Link>
                     </div>
                 <div className="text-left font-bold w-full mb-10">
 
