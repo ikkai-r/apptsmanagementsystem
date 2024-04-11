4fded4705382d546a0ed553a1fbe608b221604bd
Jomar Delos Reyes <jomardelosreyes32@gmail.com>
2024-04-12 01:45:39 +0800

    fix: delete in node 2 and transferring data to nodes

--- server/utils/transactions.js
+++ server/utils/transactions.js
@@ -1,5 +1,5 @@
 const {connectNode} = require('./nodes.js');
-const {setIsolationLevel, insertAppointment, updateAppointment, deleteAppointment, insertLog} = require('./db.js');
+const {setIsolationLevel, insertAppointment, updateAppointment, deleteAppointment, insertLog, searchAppointment} = require('./db.js');
 const luzon_regions = ['National Capital Region (NCR)', 'Ilocos Region (I)', 'Cagayan Valley (II)', 'Central Luzon (III)', 'CALABARZON (IV-A)', 'MIMAROPA (IV-B)', 'Cordillera Administrative Region (CAR)']
 
 const transactionFunc =  {
@@ -10,9 +10,10 @@ const transactionFunc =  {
 
     //TODO: Not sure if needed
     //await setIsolationLevel(centralNodeConnection, "READ UNCOMMITTED");
-    
-    const regionname = appointment.regionname;
+        
+    console.log(appointment)
 
+    const regionname = appointment.regionname;
     if(luzon_regions.includes(regionname)) {
         nodeConnection = await connectNode(2);
         nodeInvolved = 2;
@@ -72,7 +73,74 @@ const transactionFunc =  {
     
     return true;
   }, 
-  
+
+  performTransactionTest: async (appointment, type, nodeConnection) => {
+    const centralNodeConnection = await connectNode(1);
+    let nodeInvolved = 1;
+    let [rows] = [];
+
+    try {
+        if (centralNodeConnection) {
+            try {
+                if (type === 'INSERT') {
+                    await insertAppointment(appointment, centralNodeConnection);
+                } else if (type === 'UPDATE') {
+                    await updateAppointment(appointment, centralNodeConnection);
+                } else if (type === 'DELETE') {
+                    await deleteAppointment(appointment, centralNodeConnection);
+                }
+            } catch (err) {
+                console.log('Perform transaction log: ', err);
+                console.log(err);
+            }
+        } else if (!centralNodeConnection && nodeConnection) {
+            try {
+                await insertLog(nodeConnection, appointment, type, 1);
+            } catch (err) {
+                console.log('Perform transaction inserting log: ', err);
+            } 
+        } else {
+            console.log('Nodes are down.');
+        }
+    
+        if (nodeConnection) {
+            try {
+                if (type === 'INSERT') {
+                    await insertAppointment(appointment, nodeConnection);
+                } else if (type === 'UPDATE') {
+                    await updateAppointment(appointment, nodeConnection);
+                } else if (type === 'DELETE') {
+                    await deleteAppointment(appointment, nodeConnection);
+                }
+            } catch (err) {
+                console.log('Perform transaction: ', err);
+            } 
+        } else if (!nodeConnection && centralNodeConnection) {
+            try {
+                await insertLog(centralNodeConnection, appointment, type, nodeInvolved);
+            } catch (err) {
+                console.log('Perform transaction inserting log: ', err);
+            } 
+        } else {
+            console.log('Nodes are down.');
+        }
+        
+        try {
+            rows = await searchAppointment(appointment, nodeConnection);
+        } catch (err) {
+            console.log("Perform transaction selecting appointment", err);
+        }
+
+        if (nodeConnection) nodeConnection.release();
+        if (centralNodeConnection) centralNodeConnection.release();
+        
+        return rows;
+    } catch(error) {
+        console.log("error")
+        return null;
+    }
+  },
+
   performTransactionFromLog: async (appointment, type, nodeConnection) => {
 
     //TODO: Not sure if needed
