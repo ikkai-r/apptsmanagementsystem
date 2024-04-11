a66e16e29cb54b300e992b53821676d9b986c11a
koko-tsuu <colleen_tan@dlsu.edu.ph>
2024-04-11 19:28:07 +0800

    feat: case3 and case4 for global failure

--- server/utils/failover/case3.js
+++ server/utils/failover/case3.js
@@ -1,54 +1,174 @@
 const {connectNode} = require("../nodes.js")
-const {makeTransactionWithSleep, selectAppt} = require("../db.js")
+const dayjs = require('dayjs')
+const dbFuncs = require('../db.js');
 const {replicateCentralNode} = require("../../replication.js");
+const luzon_regions = ['National Capital Region (NCR)', 'Ilocos Region (I)', 'Cagayan Valley (II)', 'Central Luzon (III)', 'CALABARZON (IV-A)', 'MIMAROPA (IV-B)', 'Cordillera Administrative Region (CAR)']
 
-// stop replicator (i assume it's already stopped)
 
-const case3Fail = async(node1, node2) =>{
+
+// custom function to simulate an error in performing transaction
+async function updateAppointmentError (appointment, node) {
+    const timequeued = appointment.timequeued !== '' ? dayjs(appointment.timequeued).format('YYYY-MM-DD HH:mm:ss') : null;
+    const queuedate = appointment.queuedate !== '' ? dayjs(appointment.queuedate).format('YYYY-MM-DD HH:mm:ss') : null;
+    const starttime = appointment.starttime !== '' ? dayjs(appointment.starttime).format('YYYY-MM-DD HH:mm:ss') : null;
+    const endtime = appointment.endtime !== '' ? dayjs(appointment.endtime).format('YYYY-MM-DD HH:mm:ss') : null;
+
+    try {
+      await node.beginTransaction();
+      let result = null;
+
+      const [rows] = await node.query(
+        "SELECT * FROM appointments WHERE apptid = ? FOR UPDATE;",
+        [appointment.apptid]
+      );
+
+      
+      if (rows.length > 0) {
+
+        //
+        console.log("Trigger error here");
+        throw new Error("User initiated error for Case 3")
+        [result] = await node.query("UPDATE appointments SET pxid = ?, clinicid = ?, regionname = ?, status = ?, timequeued = ?, queuedate = ?, starttime = ?, endtime = ?, type = ? WHERE apptid = ?", [
+          appointment.pxid,
+          appointment.clinicid,
+          appointment.regionname,
+          appointment.status,
+          timequeued,
+          queuedate,
+          starttime,
+          endtime,
+          appointment.type,
+          appointment.apptid
+      ]);
+      
+      
+      } else {
+        if(node.config.port == 20155) {
+          //it belonged in the luzon node before 
+          // delete from node 2
+          dbFuncs.deleteAppointment(appointment, await connectNode(2));
+          // insert to node 3
+          dbFuncs.insertAppointment(appointment, await connectNode(3));
+        } else if(node.config.port == 20154) {
+          //it belonged in the visayas/mindanao node before 
+            // delete from node 2
+            dbFuncs.deleteAppointment(appointment, await connectNode(2));
+            // insert to node 3
+            dbFuncs.insertAppointment(appointment, await connectNode(3));
+        }
+      }
+
+      await node.commit();
+      await node.release();
+
+      return result;
+    } catch (err) {
+      console.log('Error updating the data: ', err)
+      console.log("Rolled back the data.");
+      const nodeInvolved = await dbFuncs.getNodeInvolvedFromPort(node);
+      dbFuncs.insertLog(node, appointment, 'UPDATE', nodeInvolved);
+      await node.rollback(node);
+      await node.release();
+    } 
+  }
+
+// custom performTransaction
+async function performTransaction (appointment, type) {
+    const centralNodeConnection = await connectNode(1);
+    let nodeConnection = null;
+    let nodeInvolved = 1;
+
+    const regionname = appointment.regionname;
+
+    if(luzon_regions.includes(regionname)) {
+        nodeConnection = await connectNode(2);
+        nodeInvolved = 2;
+    } else {
+        nodeConnection = await connectNode(3);
+        nodeInvolved = 3;
+    }
+    
+    console.log("Node involved: " + nodeInvolved)
+    //master working
+    if (centralNodeConnection) {
+        try {
+            //if (type === 'INSERT') {
+                //insertAppointment(appointment, centralNodeConnection);
+            //} else if (type === 'UPDATE') {
+                updateAppointmentError(appointment, centralNodeConnection);
+            //} else {
+             //   deleteAppointment(appointment, centralNodeConnection);
+            //}
+        } catch (err) {
+            console.log('Perform transaction log: ', err);
+            console.log(err);
+        }
+    } else if (!centralNodeConnection && nodeConnection) {
+        try {
+            dbFuncs.insertLog(nodeConnection, appointment, type, 1);
+        } catch (err) {
+            console.log('Perform transaction inserting log: ', err);
+        } 
+    } else {
+        console.log('Nodes are down.');
+    }
+
+    if (nodeConnection) {
+        try {
+            //if (type === 'INSERT') {
+            //    insertAppointment(appointment, nodeConnection);
+            //} else if (type === 'UPDATE') {
+                dbFuncs.updateAppointment(appointment, nodeConnection);
+            //} else {
+             //   deleteAppointment(appointment, nodeConnection);
+            //}
+        } catch (err) {
+            console.log('Perform transaction: ', err);
+        } 
+    } else if (!nodeConnection && centralNodeConnection) {
+        try {
+            dbFuncs.insertLog(centralNodeConnection, appointment, type, nodeInvolved);
+        } catch (err) {
+            console.log('Perform transaction inserting log: ', err);
+        } 
+    } else {
+        console.log('Nodes are down.');
+    }
+
+    if (nodeConnection) nodeConnection.release();
+    if (centralNodeConnection) centralNodeConnection.release();
+    
+    return true;
+}
+
+const case3Fail = async() =>{
+    
     try{   
+        
         // insert record
-        const apptid = "case3";
-        const exists = await node2.query(`SELECT * FROM appointments WHERE apptid = '${apptid}';`);
-        const pxid = "case3";
-        const clinicid = "case3";
-        const regionname = "Cagayan Valley (II)";
-        const status = "case3";
-        const type = "case3";
-    
-        const timequeued = new Date().toISOString().slice(0, 19).replace('T', ' ');
-        
-
-        if (exists[0].length === 0){
-            const query = {
-                type: "INSERT",
-                statement: "INSERT INTO appointments (pxid, clinicid, regionname, timequeued, status, type, apptid) VALUES (?,?,?,?,?,?,?)",
-                value: [pxid, clinicid, regionname, timequeued, status, type, apptid]
-            }
-            await makeTransactionWithSleep(node1, 1, query, 1, apptid);
-            await makeTransactionWithSleep(node2, 2, query, 2, apptid);
+        const appoint ={
+            apptid:"delete",
+            pxid: "delete",
+            clinicid: "delete",
+            regionname:"National Capital Region (NCR)",
+            status: "Cancel " + Math.random(),
+            type:"case3",
+            timequeued: new Date().toISOString().slice(0, 19).replace('T', ' ')
         }
-        // update
-        const status2 = "Case3Consultation " + Math.random();
+       
         
-        const query2 = {
-            statement: "UPDATE appointments SET status = ? WHERE apptid = ?;",
-            type: "UPDATE",
-            value: [ status2, apptid ]
-        }
-        const getStringify = await node2.query(`SELECT * FROM appointments WHERE apptid = '${apptid}';`);
-        await node1.query(`INSERT INTO logs (type, record, node, node_from, commit) VALUES ('UPDATE', '${getStringify}',2,0);`);
-    
-        await replicateCentralNode();
+        await performTransaction(appoint, "UPDATE")
+        await replicateCentralNode(); // replicate again
 
     } catch(error){
         console.log(error);
     }
-    
 
+    
     
 
 };
 
-module.exports ={case3Fail};
+module.exports = {case3Fail};
 
 
--- server/utils/failover/case4.js
+++ server/utils/failover/case4.js
@@ -1,52 +1,173 @@
 const {connectNode} = require("../nodes.js")
-const {makeTransactionWithSleep} = require("../db.js")
+const dayjs = require('dayjs')
+const dbFuncs = require('../db.js');
 const {replicateOtherNodes} = require("../../replication.js");
+const luzon_regions = ['National Capital Region (NCR)', 'Ilocos Region (I)', 'Cagayan Valley (II)', 'Central Luzon (III)', 'CALABARZON (IV-A)', 'MIMAROPA (IV-B)', 'Cordillera Administrative Region (CAR)']
 
-// stop replicator (i assume it's already stopped)
 
-async function case4Fail(node1, node3){
-    // insert record
-    try{
-        const apptid = "case4";
-        const exists = await node1.query(`SELECT * FROM appointments WHERE apptid = '${apptid}';`);
-        const pxid = "case4";
-        const clinicid = "case4";
-        const regionname = "case4";
-        const status = "Consultation";
-        const type = "case4";
-    
-        const timequeued = new Date().toISOString().slice(0, 19).replace('T', ' ');
 
-        if(exists[0].length === 0){
-            const query = {
-                type: "INSERT",
-                statement: "INSERT INTO appointments (pxid, clinicid, regionname, timequeued, status, type, apptid) VALUES (?,?,?,?,?,?,?)",
-                value: [pxid, clinicid, regionname, timequeued, status, type, apptid]
-            }
-            await makeTransactionWithSleep(node1, 1, query, 1, apptid);
-            await makeTransactionWithSleep(node3, 3, query, 3, apptid);
+// custom function to simulate an error in performing transaction
+async function updateAppointmentError (appointment, node) {
+    const timequeued = appointment.timequeued !== '' ? dayjs(appointment.timequeued).format('YYYY-MM-DD HH:mm:ss') : null;
+    const queuedate = appointment.queuedate !== '' ? dayjs(appointment.queuedate).format('YYYY-MM-DD HH:mm:ss') : null;
+    const starttime = appointment.starttime !== '' ? dayjs(appointment.starttime).format('YYYY-MM-DD HH:mm:ss') : null;
+    const endtime = appointment.endtime !== '' ? dayjs(appointment.endtime).format('YYYY-MM-DD HH:mm:ss') : null;
+
+    try {
+      await node.beginTransaction();
+      let result = null;
+
+      const [rows] = await node.query(
+        "SELECT * FROM appointments WHERE apptid = ? FOR UPDATE;",
+        [appointment.apptid]
+      );
+
+      
+      if (rows.length > 0) {
+
+        //
+        console.log("Trigger error here");
+        throw new Error("User initiated error for Case 3")
+        [result] = await node.query("UPDATE appointments SET pxid = ?, clinicid = ?, regionname = ?, status = ?, timequeued = ?, queuedate = ?, starttime = ?, endtime = ?, type = ? WHERE apptid = ?", [
+          appointment.pxid,
+          appointment.clinicid,
+          appointment.regionname,
+          appointment.status,
+          timequeued,
+          queuedate,
+          starttime,
+          endtime,
+          appointment.type,
+          appointment.apptid
+      ]);
+      
+      
+      } else {
+        if(node.config.port == 20155) {
+          //it belonged in the luzon node before 
+          // delete from node 2
+          dbFuncs.deleteAppointment(appointment, await connectNode(2));
+          // insert to node 3
+          dbFuncs.insertAppointment(appointment, await connectNode(3));
+        } else if(node.config.port == 20154) {
+          //it belonged in the visayas/mindanao node before 
+            // delete from node 2
+            dbFuncs.deleteAppointment(appointment, await connectNode(2));
+            // insert to node 3
+            dbFuncs.insertAppointment(appointment, await connectNode(3));
         }
-            // update
-            const status2 = "Case4Consultation " + Math.random();
-            const query2 = {
-                statement: "UPDATE appointments SET status = ? WHERE apptid = ?;",
-                type: "UPDATE",
-                value: [ status2, apptid ]
-            }
-        // insert a log into central node that contains the update record, but commit = 0
-        const getStringify = await node1.query(`SELECT * FROM appointments WHERE apptid = '${apptid}';`);
-        await node3.query(`INSERT INTO logs (type, record, node, node_from, commit) VALUES ('UPDATE', '${getStringify}',1,3,0);`);
+      }
 
-        await replicateOtherNodes();
+      await node.commit();
+      await node.release();
+
+      return result;
+    } catch (err) {
+      console.log('Error updating the data: ', err)
+      console.log("Rolled back the data.");
+      const nodeInvolved = await dbFuncs.getNodeInvolvedFromPort(node);
+      dbFuncs.insertLog(node, appointment, 'DELETE', nodeInvolved);
+      await node.rollback(node);
+      await node.release();
+    } 
+  }
+
+// custom performTransaction
+async function performTransaction (appointment, type) {
+    const centralNodeConnection = await connectNode(1);
+    let nodeConnection = null;
+    let nodeInvolved = 1;
+
+    const regionname = appointment.regionname;
+
+    if(luzon_regions.includes(regionname)) {
+        nodeConnection = await connectNode(2);
+        nodeInvolved = 2;
+    } else {
+        nodeConnection = await connectNode(3);
+        nodeInvolved = 3;
+    }
+    
+    console.log("Node involved: " + nodeInvolved)
+    //master working
+    if (centralNodeConnection) {
+        try {
+            //if (type === 'INSERT') {
+                //insertAppointment(appointment, centralNodeConnection);
+            //} else if (type === 'UPDATE') {
+                dbFuncs.updateAppointment(appointment, centralNodeConnection);
+            //} else {
+             //   deleteAppointment(appointment, centralNodeConnection);
+            //}
+        } catch (err) {
+            console.log('Perform transaction log: ', err);
+            console.log(err);
+        }
+    } else if (!centralNodeConnection && nodeConnection) {
+        try {
+            dbFuncs.insertLog(nodeConnection, appointment, type, 1);
+        } catch (err) {
+            console.log('Perform transaction inserting log: ', err);
+        } 
+    } else {
+        console.log('Nodes are down.');
+    }
+
+    if (nodeConnection) {
+        try {
+            //if (type === 'INSERT') {
+            //    insertAppointment(appointment, nodeConnection);
+            //} else if (type === 'UPDATE') {
+                updateAppointmentError(appointment, nodeConnection);
+            //} else {
+             //   deleteAppointment(appointment, nodeConnection);
+            //}
+        } catch (err) {
+            console.log('Perform transaction: ', err);
+        } 
+    } else if (!nodeConnection && centralNodeConnection) {
+        try {
+            dbFuncs.insertLog(centralNodeConnection, appointment, type, nodeInvolved);
+        } catch (err) {
+            console.log('Perform transaction inserting log: ', err);
+        } 
+    } else {
+        console.log('Nodes are down.');
+    }
+
+    if (nodeConnection) nodeConnection.release();
+    if (centralNodeConnection) centralNodeConnection.release();
+    
+    return true;
+}
+
+const case4Fail = async() =>{
+    
+    try{   
+        
+        // insert record
+        const appoint ={
+            apptid:"delete",
+            pxid: "delete",
+            clinicid: "delete",
+            regionname:"mapupunta sa VisMindanao",
+            status: "random",
+            type:"Inpatient " + Math.random(),
+            timequeued: new Date().toISOString().slice(0, 19).replace('T', ' ')
+        }
        
+        
+        await performTransaction(appoint, "UPDATE")
+        await replicateOtherNodes(); // replicate again
+
     } catch(error){
         console.log(error);
-
+    }
 
     
 
-    }
 };
 
-
 module.exports ={case4Fail};
+
+
